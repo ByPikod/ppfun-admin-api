df3c812296558243f5265eb2b4e8212d93981d50
Yahya Batulu <yahyabatulu00@gmail.com>
2023-08-26 21:02:03 +0300

    GPR

---
+++ .github/workflows/gpr.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const file = fs.readFileSync("./package.json", {
+  encoding: "utf-8",
+});
+const json = JSON.parse(file);
+json.name = "@ByPikod/ppfun-admin-api";
+fs.writeFileSync("./package.json", JSON.stringify(json, undefined, 2));
